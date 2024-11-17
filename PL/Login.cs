5686682a9aa48392036959369d5028dc7e60b2b0
Ducci <2251010020duc@ou.edu.vn>
2024-11-17 16:39:28 +0700

    Login and Signup push

---
+++ PL/Login.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PL
+{
+    public partial class Login : Form
+    {
+        public SignUp registerForm;
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2PictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2HtmlLabel4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2HtmlLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2vSeparator1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2HtmlLabel5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SignUp_label_Click(object sender, EventArgs e)
+        {
+            SignUp registerForm = new SignUp();
+            registerForm.FormClosed += (s, args) => this.Show();
+            this.Hide();
+            registerForm.Show();
+        }
+
+        private void Exit_btn_Click(object sender, EventArgs e)
+        {
+            //chứng tới nữa 
+            this.Close();
+        }
+
+        private void Username_txb_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PL/SignUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PL
+{
+    public partial class SignUp : Form
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void Username_txb_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
